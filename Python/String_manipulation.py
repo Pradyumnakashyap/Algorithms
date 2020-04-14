45064b3df9ff510f2bfb3be5dd66d90bb887b439
Pradyumnakashyap <pradyumnakas@gmail.com>
2020-04-14 12:51:02 +0200

    String manipulation practice HackerRank

---
+++ Python/String_manipulation.py
@@ -0,0 +1,13 @@
+# Complete the solve function below.
+def solve(s):
+    for x in s.split():
+        s = s.replace(x, x.capitalize())
+    return s
+
+if __name__ == '__main__':
+
+    s = input()
+
+    result = solve(s)
+
+    print(result + '\n')
