4f42a47f827eb7b3b7e5e96ee1c677b9ccbeb966
Pradyumnakashyap <pradyumnakas@gmail.com>
2020-04-24 20:15:28 +0200

    BST Level order traversal practice

---
+++ Python/BST_LOT.py
@@ -0,0 +1,43 @@
+#Binary tree insertion and level ordering (output the node values from left to right for each level starting 
+# from root )
+
+import sys
+from collections import deque
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+    def levelOrder(self,root):
+        if root is None:
+            return
+        else:
+            queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            print(node.data, end=" ")
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
